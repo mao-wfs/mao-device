9ec51cb2d208926076c6811875f4ad6b52448ff0
Scizor-master <tueda1207@gmail.com>
2018-06-19 18:04:33 +0900

    Modified.

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
+#!/usr/bin/env python
 # coding: utf-8
 from setuptools import setup
 
 setup(
     name='maoDevice',
-    version='0.0.1',
+    version='0.1',
     description='Python3.6 package to control MAO devices.',
+    url='https://github.com/mao-wfs/mao-device',
     author='Tetsutaro Ueda',
     author_email='tueda1207@gmail.com',
-    install_requires=['pyserial>=3.4']
-    url='https://github.com/mao-wfs/mao-device',
-    # packages=find_packages(exclude=('tests', 'docs')),
-    packages=['maodevice']
-    test_suite='tests',
+    packages=['maodevice'],
+    install_requires=['pyserial>=3.4'],
+    classifiers=['Programing Language :: Python :: 3.6'],
 )

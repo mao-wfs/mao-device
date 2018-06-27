ae03e7a8ef9feb901dcaea613047e6fefdcda61d
Scizor-master <tueda1207@gmail.com>
2018-06-28 02:23:03 +0900

    Modified docstring.

--- maodevice/device/device.py
+++ maodevice/device/device.py
@@ -1,33 +1,29 @@
 # coding: utf-8
 class Device(object):
-    """The base class to control devices.
-    
-    This is the base class for controlling devices.
-    Various classes to control devices are created inheriting this class.
+    """Control a device.
 
+    This is the base class of device controller.
+    
     Note:
-        You do not use this class itself. You override this class
-        and create a class corresponding to each device. 
+        This class itself is not used, but it is inherited by
+        child classes and used.
+
+    Args:
+        com: Communicator instance to control the device.
 
     Attributes:
         manufacturer (str): Manufacturer of the device.
         product_name (str): Name of the device.
         classification (str): Classification of the device.
-        com (communicator.Communicator): Communicator for controlling the device.
         _shortcut_command (dict): Dictionary of methods and its shortcuts.
     """
     manufacturer = ''
     product_name = ''
     classification = ''
-    com = None
+
     _shortcut_command = {}
 
     def __init__(self, com):
-        """Initialize 'Device'.
-
-        Args:
-            com (communicator.Communicator): Communicator to control a device.
-        """
         self.com = com
         self.com.open()
         self._add_shorcut_command()
@@ -37,9 +33,12 @@ class Device(object):
         
         Note:
             This method is only for the internal use.
-            User can add shortcuts by editting '_shortcut_command'.
+            You can add shortcuts by editting '_shortcut_command'.
+
+        Return:
+            None
         """
-        __items = self._shortcut_command.items()
-        for shortcut, method in __items:
+        _items = self._shortcut_command.items()
+        for shortcut, method in _items:
             self.__setattr__(shortcut, self.__getattribute__(method))
         return

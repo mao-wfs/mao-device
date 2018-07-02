02bd14797cc926142030a4c6c7e746eb8ecebd48
Scizor-master <tueda1207@gmail.com>
2018-07-03 06:46:59 +0900

    Modified "Python3.6" -> "Python3.7".

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup
 setup(
     name='maoDevice',
     version='0.1',
-    description='Python3.6 package to control MAO devices.',
+    description='Python3.7 package to control MAO devices.',
     url='https://github.com/mao-wfs/mao-device',
     author='Tetsutaro Ueda',
     author_email='tueda1207@gmail.com',
     packages=['maodevice'],
     install_requires=['pyserial>=3.4'],
-    classifiers=['Programing Language :: Python :: 3.6'],
+    classifiers=['Programing Language :: Python :: 3.7'],
 )

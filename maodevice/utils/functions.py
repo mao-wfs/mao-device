249b6e9b80a1c66946731ba16001713944f84e54
megascizor <tueda1207@gmail.com>
2018-07-13 03:27:12 +0900

    Add functions for bitwise operations.

---
+++ maodevice/utils/functions.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+def or_of_bits(*bits):
+    """OR the given bits.
+
+    Args:
+        bits (int)
+
+    Return:
+        or_bit (int): OR of the given bits.
+
+    Example:
+        >>> or_of_bits(1, 4, 16)
+        21 # 0b10101, 0x15
+        >>> or_of_bits(0b00010, 0b10000)
+        18 # 0b10010, 0x12
+        >>> or_of_bits(0x01, 0x10)
+        17 # 0b10001, 0x11
+    """
+    assert all(isinstance(bit, int) for bit in bits), \
+        "All elements of the given arguments must be `int`."
+
+    or_bit = 0
+    for bit in bits:
+        or_bit = or_bit | bit
+
+    return or_bit
+
+
+def extract_bits(bit, bit_dict):
+    """Extract bits which is turend on (1).
+
+    Args:
+        bit (int): Bit to check.
+        bit_dict (dict): Correspondance dict of bit and status.
+
+    Return:
+        valid_bit (:obj:`list` of :obj:`str`): List of bit which is
+            turned on (1).
+
+    Example:
+        >>> sample_dict = {
+        ...     'S1': 0b001,
+        ...     'S2': 0b010,
+        ...     'S3': 0b100,
+        ... }
+        >>> extract_bits(0b101, correlation_dict)
+        ['S1', 'S3']
+    """
+    assert isinstance(bit, int), "'or_bit' must be `int`."
+
+    assert all(isinstance(val, int) for val in bit_dict.values()), \
+        "All elements of 'bit_dict' must be `int`."
+
+    valid_bit = [
+        key for key, val in bit_dict.items() if not bit & val == 0
+    ]
+    return valid_bit

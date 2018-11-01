ef8906e1fa1f8946f35d4f2b1084126f73589d27
scizorman <tueda1207@gmail.com>
2018-11-01 18:37:27 +0900

    add : rayout of exceptions

---
+++ maodevice/exceptions.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from maodevice.core import BaseDeviceError
+
+
+# OCTAD-S (Elecs, Inc.)
+# NOTE: TBD
+class OctadSError(BaseDeviceError):
+    """Error class for "OCTAD-S"
+
+    This class is based on "maodevice.core.BaseDeviceError".
+    """
+    pass
+
+
+# Model 3390 Arbitrary Waveform Generator (Keithley Instruments, Inc.)
+# NOTE: TBD
+class Model3390AWGError(BaseDeviceError):
+    """Error class for "Model 3390 Arbitrary Waveform Generator".
+
+    This class is based on "maodevice.core.BaseDeviceError".
+    """
+    pass
+
+
+# RFLL-20-H (Optilab, LLC.)
+# NOTE: TBD
+class Rfll20HError(BaseDeviceError):
+    """Error class for "RFLL-20-H".
+
+    This class is based on "maodevice.core.BaseDeviceError".
+    """
+    pass
